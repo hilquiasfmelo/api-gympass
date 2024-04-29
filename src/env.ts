51c4bcc4dc605f7bd041e0f5e3c6861d1cf803af
Hilquias Ferreira Melo <hilquiasfmelo@hotmail.com>
2024-04-29 05:28:06 -0300

    feat: Estrutura do projeto

---
+++ src/env.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  // coerce => Busca algum dado e converte para um formato dele.
+  PORT: z.coerce.number().default(3333),
+})
+
+// safeParse => Tenta fazer a validação para saber se existem essas informações das variáveis.
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('❌ Variáveis de ambiente inválidas.', _env.error.format())
+
+  throw new Error('❌ Variáveis de ambiente inválidas.')
+}
+
+export const env = _env.data
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`🚀 Servidor em execução na porta ${env.PORT}.`)
+  })
